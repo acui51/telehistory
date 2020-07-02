5f09619309371b106261a54c61c50babc3527a75
Alix Cui <acui@stanford.edu>
2020-07-02 18:35:17 -0500

    Updates

---
+++ precache-manifest.af864eb1b00a7fa57f10bd98d0784f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a9ee76d8c5645dc05593754c87f18d",
+    "url": "/telehistory/index.html"
+  },
+  {
+    "revision": "c7e055bfef53b807fb40",
+    "url": "/telehistory/static/css/main.0f8f3854.chunk.css"
+  },
+  {
+    "revision": "f1c0cba3b76f85cd4929",
+    "url": "/telehistory/static/js/2.af1fec95.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/telehistory/static/js/2.af1fec95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e055bfef53b807fb40",
+    "url": "/telehistory/static/js/main.47c24364.chunk.js"
+  },
+  {
+    "revision": "a067a348bcf683cf9ba8",
+    "url": "/telehistory/static/js/runtime-main.f675179b.js"
+  }
+]);
